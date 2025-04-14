f5ef47f01ca4b3482c61f85be3d6585e1c447605
Nahiyat Khan <nahiyat2003@gmail.com>
2025-04-14 18:46:19 +0600

    project created

---
+++ PastryShop_2310003_2311945_2311197_23104563/src/main/java/cse213/pastryshop_2310003_2311945_2311197_23104563/HelloController.java
@@ -0,0 +1,14 @@
+package cse213.pastryshop_2310003_2311945_2311197_23104563;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ PastryShop_2310003_2311945_2311197_23104563/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module cse213.pastryshop_2310003_2311945_2311197_23104563 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens cse213.pastryshop_2310003_2311945_2311197_23104563 to javafx.fxml;
+    exports cse213.pastryshop_2310003_2311945_2311197_23104563;
+}
