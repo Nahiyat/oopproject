75d265f0fd17fe4288f87ae60141c944a2780a1f
mahatab15 <2311945@iub.edu.bd>
2025-04-22 04:03:03 +0600

    Add files via upload

---
+++ PastryShop_2310003_2311945_2311197_23104563/src/main/java/cse213/pastryshop_2310003_2311945_2311197_23104563/Mahatab/RiderDashboardController.java
@@ -0,0 +1,46 @@
+package cse213.pastryshop_2310003_2311945_2311197_23104563.Mahatab;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class RiderDashboardController {
+
+    @FXML
+    private Button cancelAnOrderButton;
+
+    @FXML
+    private ComboBox<?> daysOfLeave;
+
+    @FXML
+    void cancelAnOrderBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void contactSupportBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageAccountBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void markAsDeliveredBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void proceedToFeedbackBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void proceedToXBtn(ActionEvent event) {
+
+    }
+
+}
---
+++ PastryShop_2310003_2311945_2311197_23104563/src/main/java/cse213/pastryshop_2310003_2311945_2311197_23104563/Mahatab/RiderFeedbackController.java
@@ -0,0 +1,13 @@
+package cse213.pastryshop_2310003_2311945_2311197_23104563.Mahatab;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RiderFeedbackController {
+
+    @FXML
+    void goBackBtn(ActionEvent event) {
+
+    }
+
+}
